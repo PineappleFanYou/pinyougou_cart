4f11ebfb9254163a8fa75878abaf50489ee6927a
范优 <876255983@qq.com>
2019-07-18 18:59:15 +0800

    完成计算总数和总价的功能

--- js/cart.js
+++ js/cart.js
@@ -54,4 +54,31 @@ $(function () {
     //动态生成结构
     $('.item-list').html(html);
   }
+
+  //计算总的商品件数和总价，用一个函数封装，到时候直接调用
+  function totalCountAndMoney() {
+    // 算出总计里面的总数量和总价
+    // 根据选中的多选框，得到选中的商品的id
+    let totalCount = 0;
+    let totalMoney = 0;
+    $('.item-list .item-ck:checked').each((i, e) => {
+      // console.log(e);  e 是每个元素
+      // 我们这里找遍历，然后找到每个元素的父元素的id，我们这样做是因为等一下我们点击删除的时候，是删除整个我们动态生成的结构，和删除本地存储
+      let id = parseInt($(e).parents('.item').attr('data-id')); /* 得到的是字符串，我们转为数字 attr方法   jq对象.attr(属性名)，一个值是获取 */
+      // console.log(id);
+      // 得到父元素的id之后，我们就遍历，然后判断
+      arr.forEach(e => {
+        if (e.pID == id) {
+          // 这样相加，相当于我们的for循环里面算和，在外面声明 let sum = 0, 里面是  sum += i; 一样的
+          totalCount += e.number;
+          totalMoney += e.number * e.price;
+        }
+      });
+    })
+    //修改总件数
+    $('.selected').text(totalCount);
+    //修改总价
+    $('.total-money').text(totalMoney);
+  }
+  totalCountAndMoney();
 });
