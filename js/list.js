1257043b509303d2bf5892158675c1ddf8eb99c4
范优 <876255983@qq.com>
2019-07-18 13:10:40 +0800

    完成动态页面结构

---
+++ js/list.js
@@ -0,0 +1,32 @@
+//先来一个入口函数
+$(function () {
+    //动态生成结构
+    let html = '';
+    //遍历
+    phoneData.forEach(e => {
+        html += `<li class="goods-list-item">
+        <a href="detail.html">
+          <div class="item-img">
+            <img src="${e.imgSrc}" alt="">
+          </div>
+          <div class="item-title">
+            ${e.name}
+          </div>
+          <div class="item-price">
+            <span class="now">${`￥`+ e.price}</span>
+          </div>
+          <div class="sold">
+            <span> 已售 <em>${e.percent}</em></span>
+            <div class="scroll">
+              <div class="per" style = width:${e.percent}%></div>
+            </div>
+            <span>剩余<i>${e.left}</i>件</span>
+          </div>
+        </a>
+        <a href="#" class="buy">
+          查看详情
+        </a>
+      </li>`
+    });
+    $('.goods-list ul').html(html);
+});
